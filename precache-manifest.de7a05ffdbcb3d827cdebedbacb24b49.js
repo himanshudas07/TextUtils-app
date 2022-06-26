f37906e6d3b86edc6ee2bc0b932dcf21db26e156
himanshudas07 <himanshu.das2@gmail.com>
2022-06-26 10:53:57 +0530

    Updates

---
+++ precache-manifest.de7a05ffdbcb3d827cdebedbacb24b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3381e0dbb5dad31f5748",
+    "url": "/TextUtils-app/static/css/main.98023972.chunk.css"
+  },
+  {
+    "revision": "3381e0dbb5dad31f5748",
+    "url": "/TextUtils-app/static/js/main.3381e0db.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "d9964dae577db6ae6a4a",
+    "url": "/TextUtils-app/static/js/runtime~main.d9964dae.js"
+  },
+  {
+    "revision": "46ae5003b74e541e7012ced3179fe6ee",
+    "url": "/TextUtils-app/index.html"
+  }
+];
